aa28134f82c849519c0d38fb21ae1610aa90e4ef
okeomasilchi <okeomasilachi@gmail.com>
2024-04-03 23:49:58 +0100

    Refactor frequency variable name to freq

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -12,7 +12,7 @@ class LFUCache(BaseCaching):
     def __init__(self):
         """ Initialize """
         super().__init__()
-        self.frequency = defaultdict(int)
+        self.freq = defaultdict(int)
 
     def put(self, key, item):
         """ Add an item to the cache """
@@ -20,29 +20,29 @@ class LFUCache(BaseCaching):
             return
 
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            least_freq = min(self.frequency.values())
+            LFU = min(self.freq.values())
             keys_least_freq = [
-              k for k, v in self.frequency.items()
-              if v == least_freq
+              k for k, v in self.freq.items()
+              if v == LFU
               ]
 
             if len(keys_least_freq) == 1:
-                discarded_key = keys_least_freq[0]
+                _key = keys_least_freq[0]
             else:
-                lru_key = min(self.cache_data, key=self.frequency.get)
-                discarded_key = lru_key
+                lru_key = min(self.cache_data, key=self.freq.get)
+                _key = lru_key
 
-            del self.cache_data[discarded_key]
-            del self.frequency[discarded_key]
-            print("DISCARD:", discarded_key)
+            del self.cache_data[_key]
+            del self.freq[_key]
+            print("DISCARD:", _key)
 
         self.cache_data[key] = item
-        self.frequency[key] += 1
+        self.freq[key] += 1
 
     def get(self, key):
         """ Get an item from the cache """
         if key is None or key not in self.cache_data:
             return None
 
-        self.frequency[key] += 1
+        self.freq[key] += 1
         return self.cache_data[key]
