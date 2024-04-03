07d5e5d04b8da62e3a981ac5eb131003965240d2
okeomasilchi <okeomasilachi@gmail.com>
2024-04-03 23:30:39 +0100

    Add MRUCache class that implements a basic caching mechanism using LIFO policy

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,1 +1,55 @@
 #!/usr/bin/env python3
+"""
+BasicCache that inherits from BaseCaching and is a caching system
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    This class likely represents a basic
+    caching mechanism in Python.
+    """
+    def __init__(self):
+        """Call the constructor of the parent class"""
+        super().__init__()
+        self.vals = []
+
+    def put(self, key: str, item: str) -> None:
+        """
+        Add an item to the cache using LIFO caching policy.
+
+        Args:
+          key (str): The key to associate with
+            the item.
+          item (any): The item to be stored in
+            the cache.
+
+        Returns:
+          None
+        """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            lifo = list(self.cache_data.keys())[-1]
+            print("DISCARD:", lifo)
+            del self.cache_data[lifo]
+        self.cache_data[key] = item
+
+    def get(self, key: str) -> dict:
+        """
+        Retrieve the value associated with the given
+        key from the cache.
+
+        Args:
+          key (str): The key to retrieve the value for.
+
+        Returns:
+          dict: The value associated with the key, or
+            None if the key is not found in the cache.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        data = self.cache_data.pop(key)
+        self.cache_data[key] = data
+        return self.cache_data[key]
