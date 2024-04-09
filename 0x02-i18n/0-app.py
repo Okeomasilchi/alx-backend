0f164b0071eaec777fecf656c5d18717263268f7
okeomasilchi <okeomasilachi@gmail.com>
2024-04-09 16:57:33 +0100

    Add internationalization support to Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 """
-
+Basic Flask app
 """
 
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Returns a string"""
+    return render_template(
+        '0-index.html',
+        title='Welcome to Holberton',
+        text='Hello world'
+        )
+
+
+if __name__ == '__main__':
+    app.run()
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,5 +1,36 @@
 #!/usr/bin/env python3
 """
-
+Basic Babel setup
 """
 
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Config class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Returns a string"""
+    return render_template(
+        '1-index.html',
+        title='Welcome to Holberton',
+        text='Hello world'
+    )
+
+
+if __name__ == '__main__':
+    app.run()
