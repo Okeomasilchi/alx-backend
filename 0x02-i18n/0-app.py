f0fe5f503b0076e74b652de4a099d58bf401d140
okeomasilchi <okeomasilachi@gmail.com>
2024-04-09 17:00:20 +0100

    Refactor Flask app to use Babel for internationalization

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -13,11 +13,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def index():
     """Returns a string"""
-    return render_template(
-        '0-index.html',
-        title='Welcome to Holberton',
-        text='Hello world'
-        )
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
