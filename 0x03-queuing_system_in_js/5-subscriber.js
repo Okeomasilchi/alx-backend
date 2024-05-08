2d82aa638e2c03e4313c34ad99d668f8840fb34e
okeomasilachi onyedibia <110942635+okeomasilachi@users.noreply.github.com>
2024-05-08 08:54:52 +0000

    chore: Update Redis client and operations in queuing system code

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,33 +1,20 @@
 import redis from 'redis';
 
-// Create a Redis subscriber client
-const subscriber = redis.createClient();
+const subscriber = redis.createClient ();
 
-// Subscribe to the 'holberton school channel'
-subscriber.on('subscribe', (channel) => {
-    console.log(`Subscribed to channel: ${channel}`);
-});
-
-// Log when connected to Redis server
-subscriber.on('connect', () => {
-    console.log('Redis client connected to the server');
-});
-
-// Log error if connection to Redis server fails
-subscriber.on('error', (err) => {
-    console.error(`Redis client not connected to the server: ${err.message}`);
-});
-
-// Handle messages received on the subscribed channel
-subscriber.on('message', (channel, message) => {
-    console.log(`Received message on channel ${channel}: ${message}`);
-
-    // Check for 'KILL_SERVER' message to unsubscribe and quit
+subscriber
+  .on ('connect', () => {
+    console.log ('Redis client connected to the server');
+  })
+  .on ('error', err => {
+    console.error (`Redis client not connected to the server: ${err.message}`);
+  })
+  .on ('message', (channel, message) => {
+    console.log (message);
     if (message === 'KILL_SERVER') {
-        subscriber.unsubscribe();
-        subscriber.quit();
+      subscriber.unsubscribe ();
+      subscriber.quit ();
     }
-});
+  });
 
-// Subscribe to the 'holberton school channel'
-subscriber.subscribe('holberton school channel');
+subscriber.subscribe ('holberton school channel');
---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,23 @@
+import Kue from 'kue';
+
+const que = Kue.createQueue ();
+
+const jobData = {
+  phoneNumber: '09123666419',
+  message: 'Hello World!',
+};
+
+const job = que.create ('push_notification_code', jobData).save ();
+
+job
+  .on ('enqueue', () => {
+    console.log (`Notification job created: ${job.id}`);
+  })
+  .on ('complete', () => {
+    console.log ('Notification job completed');
+  })
+  .on ('failed', () => {
+    console.log ('Notification job failed');
+  });
+
+// Kue.app.listen (3000);
